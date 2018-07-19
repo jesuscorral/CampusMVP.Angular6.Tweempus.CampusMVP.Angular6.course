7bf31a37b3085c3031dab6e8c3c3e8bbde338c40
Jesus Corral <corralperez@gmail.com>
2018-07-19 17:43:44 +0200

    llamandas encadenadas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
+import { Author } from './../shared/author/author.model';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/from';
 import { AuthorService } from './../shared/author/author.service';
 import { Component, OnInit } from '@angular/core';
+import { Twimp } from 'src/app/shared/twimp/twimp.model';
+import { TwimpService } from '../shared/twimp/twimp.service';
 
 @Component({
   selector: 'tweempus-dashboard',
@@ -7,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor(private atuhorService: AuthorService) { }
+  twimpList: Twimp[] = [];
+  constructor(private atuhorService: AuthorService,
+              private twimpService: TwimpService) { }
 
   ngOnInit() {
-    this.atuhorService.getAuthor('1').subscribe(author => console.log(author));
+    this.twimpService.getTwimps().subscribe(twimps => {
+      Observable.from(twimps).subscribe(twimp =>  {
+        this.atuhorService.getAuthor(twimp.author.id).subscribe(author => {
+          twimp.author = author;
+          this.twimpService.getFavoritesByAuthor('1', twimp.id).subscribe(favorite => {
+            twimp.favorite = favorite;
+            this.twimpList.push(twimp);
+          });
+        });
+      });
+    });
   }
 
 }
--- src/app/shared/twimp/twimp.service.ts
+++ src/app/shared/twimp/twimp.service.ts
@@ -1,8 +1,53 @@
+import { catchError, map } from 'rxjs/operators';
+import { Author } from './../author/author.model';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Twimp } from './twimp.model';
+
 
 @Injectable()
 export class TwimpService {
 
-  constructor() { }
+  private url: string = 'http://localhost:3000/twimps';
+  private urlFavorite: string = 'http://localhost:3000/author-favorites';
 
+  constructor(private httpClient: HttpClient) { }
+
+  getTwimps(): Observable<Twimp[]>{
+    let twimps: Twimp[] = [];
+
+    return this.httpClient.get(this.url).pipe(
+      map(dbTwimpList => {
+        for(let i in dbTwimpList) {
+          let twimp: Twimp = new Twimp(dbTwimpList[i].id, 'localhost:4200/twimp/' + dbTwimpList[i].id, new Author(dbTwimpList[i].author), dbTwimpList[i].content, dbTwimpList[i].timestamp);
+          twimps.push(twimp);
+        }
+        return twimps;
+      }),
+      catchError(this.handleError)
+    )
+  }
+
+  getFavoritesByAuthor(idAuthor: string, idTwimp: string): Observable<boolean> {
+    return this.httpClient.get(this.urlFavorite + '/' + idAuthor).pipe(
+      map(response => {
+        let favorites: string[] = response['twimps'];
+        if(favorites.indexOf(idTwimp) == -1) {
+          return false;         
+        }
+        else {
+          return true;
+        }
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
